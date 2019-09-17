fba223d07d74b0c3c6e806433e9074234a48afa6
elyeel <elyeel@yahoo.com>
2019-09-17 04:58:02 +0000

    exercise 7 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,11 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-Store.has_many :employees
-Employee.belongs_to :Store
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Naoko", last_name: "Sinatra", hourly_rate: 40)
-@store2.employees.create(first_name: "Gloria", last_name: "Supadi", hourly_rate: 30)
+@store2.employees.create(first_name: "Gloria", last_name: "Supadi", hourly_rate: 40)
 @store2.employees.create(first_name: "Tracey", last_name: "Gillcash", hourly_rate: 61)
-@store2.employees.create(first_name: "Roberto", last_name: "Garay", hourly_rate: 38)
+@store2.employees.create(first_name: "Roberto", last_name: "Garay", hourly_rate: 48)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@new_store_name = gets.chomp
+store = Store.create(name: @new_store_name)
+puts store.errors.messages
+
+samadi = Store.create(name: "Samadi", annual_revenue: -300000, mens_apparel: false, womens_apparel: false)
+puts samadi.errors.messages
+
+employee = @store1.employees.create(first_name: "Tornado", last_name: "Putar", hourly_rate: 300)
+puts employee.errors.messages
